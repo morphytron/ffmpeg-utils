ad03ccd0dcd75b10e7935197203f8c357e9dccfc
morphytron <daniel.apatiga@pickupfit.com>
2025-01-20 00:12:37 -0500

    Fixed a missing argument after refactoring.

--- convert_mp4_to_mp3.py
+++ convert_mp4_to_mp3.py
@@ -1,12 +1,32 @@
 import os, subprocess as sp
+import sys
+
 from media_io import media_io
 class create_mp3_from_folder_and_subdirectories:
+    convert_to_file_ext = "mp3"
     def __init__(self):
         med_io = media_io()
         med_io.get_args_for_read_folder()
         med_io.sort_lists()
+        self.get_args()
         self.gen_video_list(all_media_files=med_io.all_media_files, all_key_abs_paths=med_io.all_key_abs_paths,file_ext=med_io.file_ext, file_ext_len=med_io.file_ext_len)
         self.execute_transcode(all_media_files=med_io.all_media_files, all_key_abs_paths=med_io.all_key_abs_paths,file_ext=med_io.file_ext, file_ext_len=med_io.file_ext_len)
+
+    def get_args(self):
+        args = sys.argv
+        args_iter = iter(args)
+        while True:
+            try:
+                nx = next(args_iter)
+                if nx == '-C' or nx == '--convert-to-ext':
+                    try:
+                        self.convert_to_file_ext = next(args_iter)
+                    except:
+                        print("Possibly no argument following -C, '--convert-to-ext'")
+                elif nx == '-h' or nx == '--help':
+                    print("-C or --convert-to-ext argument typically should be of 'mp3' format, however, it could be a different audio format supported by ffmpeg.")
+            except (StopIteration):
+                break
     def gen_video_list(self, all_media_files = {}, all_key_abs_paths = {}, file_ext = '', file_ext_len=0):
         for key in all_media_files.keys():
             list_of_vids = str(os.path.join(os.getcwd(),key + '.txt'))
@@ -14,7 +34,7 @@ class create_mp3_from_folder_and_subdirectories:
             print("Generating video list for {} @ {}".format(key, list_of_vids))
             with open(list_of_vids, 'w') as f:
                 for abs_path_fname in all_media_files[key]:
-                    write_line = 'file \'{}.{}\'\n'.format(abs_path_fname[:-file_ext_len], file_ext)
+                    write_line = 'file \'{}.{}\'\n'.format(abs_path_fname[:-(file_ext_len + 1)], self.convert_to_file_ext)
                     print("Writing {}".format(write_line))
                     f.write(write_line) # omit the last 4 chars of the absolute path , which includes mp3 and .
                 f.flush()
@@ -32,7 +52,7 @@ class create_mp3_from_folder_and_subdirectories:
                 in_args.append("\"{}\"".format(abs_path_to_mp4))
                 out_args.append("-map")
                 out_args.append(str(index % ideal_jobs_count))
-                out_args.append("\"{}\"".format(abs_path_to_mp4[:-file_ext_len] + '.'.format(file_ext)))
+                out_args.append("\"{}\"".format(abs_path_to_mp4[:-(file_ext_len + 1)] + '.{}'.format(self.convert_to_file_ext)))
                 index += 1
             while len(in_args) > 0:
                 print("Ideal number of jobs is {} if each task is 3 threads".format(ideal_jobs_count))
@@ -45,7 +65,7 @@ class create_mp3_from_folder_and_subdirectories:
                 in_args = in_args[ideal_jobs_count*2:]
                 out_args = out_args[ideal_jobs_count*3:]
             print("Starting combining to single {}...".format(file_ext))
-            args_last = " ".join(['ffmpeg', '-f', 'concat', '-safe', '0', '-i', "\"{}\"".format(self.all_key_abs_paths[key]), "\"{}\"".format(self.all_key_abs_paths[key][:-file_ext_len] + '.{}'.format(file_ext))])
+            args_last = " ".join(['ffmpeg', '-f', 'concat', '-safe', '0', '-i', "\"{}\"".format(self.all_key_abs_paths[key]), "\"{}\"".format(self.all_key_abs_paths[key][:-(file_ext_len + 1)] + '.{}'.format(self.convert_to_file_ext))])
             print(args_last)
             sp.run(args_last,
                            # Probably don't forget these, too
